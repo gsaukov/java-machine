85b5a65ae216a9537a859012346a235539af0f0a
georgy.saukov <gsaukov@yahoo.com>
2019-03-27 01:37:42 +0100

    lakescalculator total namings

--- src/main/java/com/apps/lakescalculator/LakesCalculator.java
+++ src/main/java/com/apps/lakescalculator/LakesCalculator.java
@@ -66,35 +66,33 @@ public class LakesCalculator {
     private static List<Lake> calculateLakeData(List<List<Surface>> lakes) {
         List<Lake> calculatedLakes = new ArrayList<>();
 
-        int aggregatedVolume = 0;
-        int aggregatedMirror = 0;
-        int overallMaxDepth = 0;
-        int overallMaxHeight = 0;
+        int totalVolume = 0;
+        int totalMirror = 0;
+        int totalMaxDepth = 0;
+        int totalMaxHeight = 0;
 
         for(List<Surface> lake : lakes){
             int leftBank = lake.get(0).val;
             int rightBank = lake.get(lake.size() -1).val;
             int seaLevel = leftBank < rightBank ? leftBank : rightBank;
             int maxHeight = leftBank < rightBank ? rightBank : leftBank;
+            totalMaxHeight = totalMaxHeight < maxHeight ? maxHeight : totalMaxHeight;
             int maxDepth = 0;
             int mirror = 0;
             int volume = 0;
             for (Surface surface : lake) {
                 surface.depth = seaLevel > surface.val ? seaLevel - surface.val : 0;
                 volume += surface.depth;
-                aggregatedVolume += surface.depth;
+                totalVolume += surface.depth;
                 if(surface.depth > 0){
                     mirror += 1;
-                    aggregatedMirror += 1;
+                    totalMirror += 1;
                     if(surface.depth > maxDepth){
                         maxDepth = surface.depth;
-                        if(surface.depth > overallMaxDepth){
-                            overallMaxDepth = surface.depth;
+                        if(surface.depth > totalMaxDepth){
+                            totalMaxDepth = surface.depth;
                         }
                     }
-                }
-                if(overallMaxHeight < maxHeight){
-                    overallMaxHeight = maxHeight;
                 }
             }
             if(volume > 0){// bye bye right downhill dry lake
@@ -112,12 +110,12 @@ public class LakesCalculator {
         }
         if(!calculatedLakes.isEmpty()){
             calculatedLakes.add(new Lake.Builder()
-                    .withVolume(aggregatedVolume)
-                    .withMirror(aggregatedMirror)
-                    .withMaxDepth(overallMaxDepth)
-                    .withMaxHeight(overallMaxHeight)
-                    .withAverageArithmeticDepth((float)aggregatedVolume / aggregatedMirror)
-                    .withFormFactor((float)aggregatedMirror / aggregatedVolume)
+                    .withVolume(totalVolume)
+                    .withMirror(totalMirror)
+                    .withMaxDepth(totalMaxDepth)
+                    .withMaxHeight(totalMaxHeight)
+                    .withAverageArithmeticDepth((float)totalVolume / totalMirror)
+                    .withFormFactor((float)totalMirror / totalVolume)
                     .setTotal(true)
                     .build());
 
