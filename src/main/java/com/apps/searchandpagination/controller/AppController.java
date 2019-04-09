0ac4141bfc24fb3f5acad27c46f4f4680d03a9d1
georgy.saukov <gsaukov@yahoo.com>
2019-04-09 23:58:29 +0200

    searchandpagination: good pagination

--- src/main/java/com/apps/searchandpagination/controller/AppController.java
+++ src/main/java/com/apps/searchandpagination/controller/AppController.java
@@ -9,11 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Controller
 public class AppController {
@@ -29,21 +25,11 @@ public class AppController {
         int currentPage = optCurrentPage.orElse(0);
         int pageSize = size.orElse(10);
 
-        Page<DataObject> dataPage = service.findPaginated(PageRequest.of(currentPage - 0, pageSize));
+        Page<DataObject> dataPage = service.findPaginated(PageRequest.of(currentPage, pageSize));
 
-//        Page<DataObject> agentePage = agenteService.findAll(pageable);
         PageWrapper<DataObject> page = new PageWrapper<DataObject>(dataPage, "/");
         model.addAttribute("page", page);
-
         model.addAttribute("dataPage", dataPage);
-
-//        int totalPages = dataPage.getTotalPages();
-//        if (totalPages > 0) {
-//            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-//                    .boxed()
-//                    .collect(Collectors.toList());
-//            model.addAttribute("pageNumbers", pageNumbers);
-//        }
 
         return "datatable";
     }
