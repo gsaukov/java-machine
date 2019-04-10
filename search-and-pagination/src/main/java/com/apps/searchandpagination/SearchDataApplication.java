56113f15ae0bdd1c4d61e72cb70f2f08cd9ec755
georgy.saukov <georgy.saukov@wirecard.com>
2019-04-10 23:29:54 +0200

    Big split

---
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/SearchDataApplication.java
@@ -0,0 +1,14 @@
+package com.apps.searchandpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class SearchDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchDataApplication.class, args);
+    }
+
+}
