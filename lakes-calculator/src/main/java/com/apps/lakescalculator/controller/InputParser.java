591843fdeb3d1d4511652b66035eda911819943c
georgy.saukov <gsaukov@yahoo.com>
2019-04-11 00:29:39 +0200

    lakescalculator: fixed number format

--- lakes-calculator/src/main/java/com/apps/lakescalculator/controller/InputParser.java
+++ lakes-calculator/src/main/java/com/apps/lakescalculator/controller/InputParser.java
@@ -2,6 +2,8 @@ package com.apps.lakescalculator.controller;
 
 import org.springframework.stereotype.Service;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 @Service
 public class InputParser {
 
@@ -16,11 +18,13 @@ public class InputParser {
 
     public String fixInput(String s){
         if(s.length() > 3){
-            return s.substring(0, Math.min(s.length(), 3));
-        } else if (s.length() == 0) {
-            return "0";
-        } else {
+            s = s.substring(0, Math.min(s.length(), 3));
+        }
+
+        if (isNumeric(s)){
             return s;
+        } else {
+            return "0";
         }
     }
 }
