ac2951566cef2f5dfaf1290d128093cd32749f64
georgy.saukov <gsaukov@yahoo.com>
2019-06-04 00:20:08 +0200

    searchandpagination: SearchCache

--- search-and-pagination/src/main/java/com/apps/searchandpagination/service/SearchKeeper.java
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/service/SearchKeeper.java
@@ -1,26 +1,34 @@
 package com.apps.searchandpagination.service;
 
 import com.apps.searchandpagination.persistance.query.trade.TradeDetailsCriteria;
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SearchKeeper {
-    private final ConcurrentHashMap<String, TradeDetailsCriteria> keeper = new ConcurrentHashMap<>();
-    private final CacheBuilder cacheBuilder = CacheBuilder.newBuilder();
+    private final Cache<String, TradeDetailsCriteria> cache;
+
+    public SearchKeeper() {
+        this.cache =  CacheBuilder.newBuilder()
+                .expireAfterAccess(180, TimeUnit.MINUTES)
+                .maximumSize(4096)
+                .concurrencyLevel(3)
+                .build();
+    }
+
 
     public String addSearchCriteria(TradeDetailsCriteria criteria){
-//        cacheBuilder.
         String id = UUID.randomUUID().toString();
-        keeper.put(id, criteria);
+        cache.put(id, criteria);
         return id;
     }
 
     public Optional<TradeDetailsCriteria> getSearchCriteria(String searchId){
-        return Optional.ofNullable(keeper.get(searchId));
+        return Optional.ofNullable(cache.getIfPresent(searchId));
     }
 }
