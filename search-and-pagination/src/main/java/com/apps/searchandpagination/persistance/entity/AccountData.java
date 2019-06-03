5fdfaa1bdb962596710d16651d8023c5da84d7f4
georgy.saukov <gsaukov@yahoo.com>
2019-06-04 01:08:19 +0200

    searchandpagination: Playing With map and account

---
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/persistance/entity/AccountData.java
@@ -0,0 +1,25 @@
+package com.apps.searchandpagination.persistance.entity;
+
+public class AccountData {
+
+    private String accountId;
+    private String limitations;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String cCEmails;
+    private String organization;
+    private String phone;
+    private String referenceID;
+    private String shippingAddress;
+    private String shippingAddress2;
+    private String shippingCountry;
+    private String shippingState;
+    private String shippingZipCode;
+    private String verifiedforACH;
+    private String eCheck;
+    private String payments;
+    private String taxExempt;
+    private String vatNumber;
+    private String maximumLimit;
+}
