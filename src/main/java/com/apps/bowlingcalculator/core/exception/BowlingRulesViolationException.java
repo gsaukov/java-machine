fa35e455852237e4e6ee084a613d4f965b8e8f6c
georgy.saukov <gsaukov@yahoo.com>
2019-06-12 23:19:06 +0200

    cleanup

--- src/main/java/com/apps/bowlingcalculator/core/exception/BowlingRulesViolationException.java
+++
@@ -1,9 +0,0 @@
-package com.apps.bowlingcalculator.core.exception;
-
-public class BowlingRulesViolationException extends IllegalArgumentException {
-
-    public BowlingRulesViolationException(String s) {
-        super(s);
-    }
-
-}
--- src/main/java/com/apps/t24app/persistance/repository/EntryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apps.t24app.persistance.repository;
-
-//@Transactional
-
-public interface EntryRepository {
-//        extends JpaRepository<Entry, String> {
-}
