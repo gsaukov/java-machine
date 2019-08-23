5229bd5b391bf2c152e2a990b457da0092844bbe
georgy.saukov <gsaukov@yahoo.com>
2019-08-23 23:41:37 +0200

    cloud: Zipkin setup

--- cloud/zipkin/src/main/java/com/apps/cloud/zipkin/ZipkinApplication.java
+++ cloud/zipkin/src/main/java/com/apps/cloud/zipkin/ZipkinApplication.java
@@ -3,6 +3,7 @@ package com.apps.cloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
