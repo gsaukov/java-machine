b319f80d6cf327a3feb396af9873167b38e88e08
georgy.saukov <gsaukov@yahoo.com>
2019-09-04 22:05:49 +0200

    cloud: auth config fix

--- cloud/justitia/src/main/java/com/apps/cloud/justitia/security/AppSecurityConfigurer.java
+++ cloud/justitia/src/main/java/com/apps/cloud/justitia/security/AppSecurityConfigurer.java
@@ -1,6 +1,5 @@
 package com.apps.cloud.justitia.security;
 
-import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
