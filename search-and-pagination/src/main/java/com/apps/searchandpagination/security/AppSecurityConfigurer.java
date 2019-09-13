5412641e18d0174ebe242bc6412f086366102745
georgy.saukov <georgy.saukov@wirecard.com>
2019-09-13 23:27:26 +0200

    cloud: CORS, CSRF support.

---
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/security/AppSecurityConfigurer.java
@@ -0,0 +1,24 @@
+package com.apps.searchandpagination.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfigurer
+        extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+    }
+
+}
