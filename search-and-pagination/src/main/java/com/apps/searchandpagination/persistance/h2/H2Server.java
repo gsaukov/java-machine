1a40efe7c2fa35c21e28e0666e5aabd957fb3426
georgy.saukov <gsaukov@yahoo.com>
2019-09-14 00:22:49 +0200

    cloud: migration to MySql

--- search-and-pagination/src/main/java/com/apps/searchandpagination/persistance/h2/H2Server.java
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/persistance/h2/H2Server.java
@@ -1,6 +1,6 @@
 package com.apps.searchandpagination.persistance.h2;
 
-import org.h2.tools.Server;
+//import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +11,9 @@ public class H2Server {
 
 //    new conn string jdbc:h2:tcp://localhost:9090/file:./data/db/spDb
 //    conn string jdbc:h2:tcp://localhost:9090/mem:spDb
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-    }
+//    @Bean(initMethod = "start", destroyMethod = "stop")
+//    public Server inMemoryH2DatabaseServer() throws SQLException {
+//        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+//    }
 
 }
