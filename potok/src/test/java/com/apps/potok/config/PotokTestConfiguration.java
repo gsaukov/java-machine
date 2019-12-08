11d60d02b33a1e650297e58063d80e373c7e3539
georgy.saukov <georgy.saukov@wirecard.com>
2019-12-08 16:04:41 +0100

    potok testing.

--- potok/src/test/java/com/apps/potok/config/PotokTestConfiguration.java
+++ potok/src/test/java/com/apps/potok/config/PotokTestConfiguration.java
@@ -1,25 +1,13 @@
 package com.apps.potok.config;
 
 import com.apps.potok.exchange.config.ServerConfigurator;
-import com.apps.potok.exchange.core.OrderManager;
-import com.apps.potok.exchange.eventhandlers.BalanceNotifierServer;
-import com.apps.potok.exchange.eventhandlers.ExecutionNotifierServer;
-import com.apps.potok.exchange.eventhandlers.PositionNotifierServer;
-import com.apps.potok.exchange.eventhandlers.QuoteNotifierServer;
 import com.apps.potok.exchange.eventhandlers.QuoteSubscribersV2;
-import com.apps.potok.exchange.init.Initiator;
-import com.apps.potok.exchange.mkdata.Route;
 import com.apps.potok.soketio.config.SpringConfig;
 import com.corundumstudio.socketio.BroadcastOperations;
 import com.corundumstudio.socketio.SocketIOServer;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
---
+++ potok/src/test/java/com/apps/potok/config/ServerTestConfigurator.java
@@ -0,0 +1,52 @@
+package com.apps.potok.config;
+
+import com.apps.potok.exchange.eventhandlers.BalanceNotifierServer;
+import com.apps.potok.exchange.eventhandlers.ExecutionNotifierServer;
+import com.apps.potok.exchange.eventhandlers.PositionNotifierServer;
+import com.apps.potok.exchange.eventhandlers.QuoteNotifierServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+@Configuration
+public class ServerTestConfigurator {
+
+    private TaskExecutor executor = new SimpleAsyncTaskExecutor();
+
+    @Autowired
+    QuoteNotifierServer quoteNotifierServer;
+
+    @Autowired
+    PositionNotifierServer positionNotifierServer;
+
+    @Autowired
+    ExecutionNotifierServer executionNotifierServer;
+
+    @Autowired
+    BalanceNotifierServer balanceNotifierServer;
+
+    public void runServers() {
+        runQuoteNotifierServer();
+        runExecutionNotifier();
+        runBalanceNotifier();
+        runPositionNotifier();
+    }
+
+    public void runQuoteNotifierServer() {
+        executor.execute(quoteNotifierServer);
+    }
+
+    public void runExecutionNotifier() {
+        executor.execute(executionNotifierServer);
+    }
+
+    public void runBalanceNotifier() {
+        executor.execute(balanceNotifierServer);
+    }
+
+    private void runPositionNotifier() {
+        executor.execute(positionNotifierServer);
+    }
+
+}
