34abed219ea08f96573998831367f9badf0e288a
georgy.saukov <gsaukov@yahoo.com>
2020-01-13 15:49:02 +0100

    authdemo facebook api

--- auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
+++ auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
@@ -1,7 +1,10 @@
 package com.apps.authdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthCodeController {
 
+    @Autowired
+    private FacebookConnectionFactory connectionFactory;
+
 //    https://localhost:8097/codeauth?code=a
 //    https://www.facebook.com/v5.0/dialog/oauth?client_id=2930956966914426&redirect_uri=https%3A%2F%2Flocalhost%3A8097%2Fcodeauth&response_type=token
+//    https://www.facebook.com/v5.0/dialog/oauth?client_id=2930956966914426&redirect_uri=https%3A%2F%2Flocalhost%3A8097%2Fcodeauth&response_type=code
     @GetMapping("/codeauth")
-    public String codeauth( @RequestParam("#access_token") String code) {
-        Facebook facebook = new FacebookTemplate(code);
+    public String codeauth( @RequestParam("code") String code) {
+        AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, "https://localhost:8097/codeauth", null); // not that the application was deployed at "http://localhost:8080/testApp"
+
+        Facebook facebook = new FacebookTemplate(accessGrant.getAccessToken(), "http://ogp.me/ns/fb#", "2930956966914426");
         return "";
     }
 
--- auth-demo/src/main/java/com/apps/authdemo/facebook/FacebookConfig.java
+++ auth-demo/src/main/java/com/apps/authdemo/facebook/FacebookConfig.java
@@ -1,5 +1,6 @@
 package com.apps.authdemo.facebook;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.social.UserIdSource;
@@ -12,9 +13,14 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 @Configuration
 public class FacebookConfig implements SocialConfigurer {
 
+    @Bean
+    public FacebookConnectionFactory facebookConnectionFactory() {
+        return new FacebookConnectionFactory("2930956966914426", "6da884e06396dd927d6f6b6d78fd5dac");
+    }
+
     @Override
     public void addConnectionFactories(ConnectionFactoryConfigurer cfConfig, Environment env) {
-        cfConfig.addConnectionFactory(new FacebookConnectionFactory("2930956966914426", "6da884e06396dd927d6f6b6d78fd5dac"));
+        cfConfig.addConnectionFactory(facebookConnectionFactory());
     }
 
     @Override
