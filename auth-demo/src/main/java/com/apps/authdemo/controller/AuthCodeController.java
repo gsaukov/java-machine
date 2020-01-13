b15e1c9e2b0f1648e04ef765f1886f1e560935fa
georgy.saukov <gsaukov@yahoo.com>
2020-01-13 16:14:34 +0100

    authdemo facebook api

--- auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
+++ auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
@@ -2,6 +2,7 @@ package com.apps.authdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
@@ -11,6 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthCodeController {
+
+    static String[] PROFILE_FIELDS_SMALL = {
+            "id", "email", "first_name", "gender", "last_name", "link", "locale", "profile_pic"
+    };
+
+    static String[] PROFILE_FIELDS = {
+            "id", "about", "age_range", "birthday", "cover", "currency", "devices", "education", "email",
+            "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type",
+            "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format",
+            "political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other",
+            "sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift",
+            "website", "work"
+    };
 
     @Autowired
     private FacebookConnectionFactory connectionFactory;
@@ -23,6 +37,8 @@ public class AuthCodeController {
         AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, "https://localhost:8097/codeauth", null); // not that the application was deployed at "http://localhost:8080/testApp"
 
         Facebook facebook = new FacebookTemplate(accessGrant.getAccessToken(), "http://ogp.me/ns/fb#", "2930956966914426");
+
+        User user = facebook.fetchObject("me", User.class, PROFILE_FIELDS);
         return "";
     }
 
