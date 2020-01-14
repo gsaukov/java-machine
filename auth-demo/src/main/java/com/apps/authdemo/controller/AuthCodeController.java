fed833fa88dad042960ecaa01eb1cd259c4fc4c6
georgy.saukov <georgy.saukov@wirecard.com>
2020-01-15 00:58:27 +0100

    authdemo slides

--- auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
+++ auth-demo/src/main/java/com/apps/authdemo/controller/AuthCodeController.java
@@ -9,13 +9,10 @@ import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthCodeController {
-
-    static String[] PROFILE_FIELDS_SMALL = {
-            "id", "email", "first_name", "gender", "last_name", "link", "locale", "profile_pic"
-    };
 
     static String[] PROFILE_FIELDS = {
             "id", "about", "age_range", "birthday", "cover", "currency", "devices", "education", "email",
@@ -32,14 +29,16 @@ public class AuthCodeController {
 //    https://localhost:8097/codeauth?code=a
 //    https://www.facebook.com/v5.0/dialog/oauth?client_id=2930956966914426&redirect_uri=https%3A%2F%2Flocalhost%3A8097%2Fcodeauth&response_type=token
 //    https://www.facebook.com/v5.0/dialog/oauth?client_id=2930956966914426&redirect_uri=https%3A%2F%2Flocalhost%3A8097%2Fcodeauth&response_type=code
+
+    @ResponseBody
     @GetMapping("/codeauth")
-    public String codeauth( @RequestParam("code") String code) {
+    public User codeauth( @RequestParam("code") String code) {
         AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, "https://localhost:8097/codeauth", null); // not that the application was deployed at "http://localhost:8080/testApp"
 
         Facebook facebook = new FacebookTemplate(accessGrant.getAccessToken(), "http://ogp.me/ns/fb#", "2930956966914426");
 
         User user = facebook.fetchObject("me", User.class, PROFILE_FIELDS);
-        return "";
+        return user;
     }
 
 }
