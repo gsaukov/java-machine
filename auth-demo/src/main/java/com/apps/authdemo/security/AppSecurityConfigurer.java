321c83181e33b15ad548c345257266119ee95b72
georgy.saukov <gsaukov@yahoo.com>
2020-01-16 02:12:49 +0100

    authdemo backend

---
+++ auth-demo/src/main/java/com/apps/authdemo/security/AppSecurityConfigurer.java
@@ -0,0 +1,75 @@
+package com.apps.authdemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfigurer
+        extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+            .antMatchers("/logedout").permitAll()
+            .anyRequest().authenticated()
+//            .and().addFilterBefore(appAuthenticationFilter(), SessionManagementFilter.class)
+            .and().logout()
+            .logoutUrl("/performlogout")
+            .logoutSuccessUrl("/logedout")
+            .invalidateHttpSession(true)
+            .deleteCookies("SESSION")
+            .and().httpBasic().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ auth-demo/src/main/java/com/apps/authdemo/security/AppUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.apps.authdemo.security;
+
+import com.apps.authdemo.data.entity.User;
+import com.apps.authdemo.data.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static java.text.MessageFormat.format;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException(format("Unknown user with username [{0}].", username));
+        }
+
+        return toAppUser(user.get());
+    }
+
+    private org.springframework.security.core.userdetails.User toAppUser(User user) {
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
+    }
+
+}
