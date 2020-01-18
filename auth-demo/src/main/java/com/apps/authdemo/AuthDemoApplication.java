5cb0e1a1b1c7b96a86088c95eb0ae03b77854b69
georgy.saukov <gsaukov@yahoo.com>
2020-01-18 01:32:23 +0100

    authdemo fooled class loader.

--- auth-demo/src/main/java/com/apps/authdemo/AuthDemoApplication.java
+++ auth-demo/src/main/java/com/apps/authdemo/AuthDemoApplication.java
@@ -1,14 +1,16 @@
 package com.apps.authdemo;
 
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class AuthDemoApplication {
 
+    public static TLSClientHelloExtractor tlsClientHelloExtractor;
+
     public static void main(String[] args) {
+        tlsClientHelloExtractor = null;
         SpringApplication.run(AuthDemoApplication.class, args);
     }
 
