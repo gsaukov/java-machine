43b001837dd6aba4452359976a7a28402cde068b
georgy.saukov <gsaukov@yahoo.com>
2020-03-11 23:40:41 +0100

    containerization docker.

--- search-and-pagination/src/main/java/com/apps/searchandpagination/cassandra/config/CassandraConfig.java
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/cassandra/config/CassandraConfig.java
@@ -1,7 +1,5 @@
 package com.apps.searchandpagination.cassandra.config;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
@@ -10,33 +8,6 @@ import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    public static final String KEYSPACE_CREATION_QUERY = "CREATE KEYSPACE IF NOT EXISTS geoKeySpace " + "WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' };";
-
-    public static final String KEYSPACE_ACTIVATE_QUERY = "USE geoKeySpace;";
-
-    public static final String KEYSPACE_CREATE_ADDRESS_DATA = "CREATE TABLE IF NOT EXISTS address_data ( " +
-            "            address_id text, " +
-            "            account_id text, " +
-            "            longitude double, " +
-            "            latitude double, " +
-            "            place_id bigint, " +
-            "            osm_type text, " +
-            "            osm_id text, " +
-            "            bounding_box map<text, double>, " +
-            "            polygon_points list <frozen <polygon>>, " +
-            "            display_name text, " +
-            "            element_class text, " +
-            "            element_type text, " +
-            "            address_elements map<text, text>, " +
-            "            name_details map<text, text>, " +
-            "            place_rank int, " +
-            "            importance double, " +
-            "            wkt text, " +
-            "            PRIMARY KEY ((address_id, account_id, longitude, latitude)) " +
-            ");";
-
-    public static final String KEYSPACE_CREATE_TYPE_POLYGON = "CREATE TYPE IF NOT EXISTS polygon (latitude double, longitude double);";
-
     @Override
     protected String getKeyspaceName() {
         return "geoKeySpace";
@@ -44,7 +15,6 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Bean
     public CassandraClusterFactoryBean cluster() {
-        startCassandraEmbedded();
         CassandraClusterFactoryBean cluster =
                 new CassandraClusterFactoryBean();
         //can be more than one contact point
@@ -53,25 +23,4 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         return cluster;
     }
 
-
-    public void startCassandraEmbedded() {
-//        try {
-//            EmbeddedCassandraServerHelper.startEmbeddedCassandra();
-//        } catch (TTransportException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (ConfigurationException e) {
-//            e.printStackTrace();
-//        }
-//        Cluster cluster = Cluster.builder().addContactPoints("127.0.0.1").withPort(9042).build();
-//        Session session = cluster.connect();
-//        session.execute(KEYSPACE_CREATION_QUERY);
-//        session.execute("DROP TABLE IF EXISTS geoKeySpace.address_data");
-//        session.execute(KEYSPACE_ACTIVATE_QUERY);
-//        session.execute(KEYSPACE_CREATE_TYPE_POLYGON);
-//        session.execute(KEYSPACE_CREATE_ADDRESS_DATA);
-    }
 }
---
+++ search-and-pagination/src/main/java/com/apps/searchandpagination/data/CassandraKeyspaceCreationTask.java
@@ -0,0 +1,69 @@
+package com.apps.searchandpagination.data;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.thrift.transport.TTransportException;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CassandraKeyspaceCreationTask implements ApplicationRunner {
+
+    public static final String KEYSPACE_CREATION_QUERY = "CREATE KEYSPACE IF NOT EXISTS geoKeySpace " + "WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '3' };";
+
+    public static final String KEYSPACE_ACTIVATE_QUERY = "USE geoKeySpace;";
+
+    public static final String KEYSPACE_CREATE_ADDRESS_DATA = "CREATE TABLE IF NOT EXISTS address_data ( " +
+            "            address_id text, " +
+            "            account_id text, " +
+            "            longitude double, " +
+            "            latitude double, " +
+            "            place_id bigint, " +
+            "            osm_type text, " +
+            "            osm_id text, " +
+            "            bounding_box map<text, double>, " +
+            "            polygon_points list <frozen <polygon>>, " +
+            "            display_name text, " +
+            "            element_class text, " +
+            "            element_type text, " +
+            "            address_elements map<text, text>, " +
+            "            name_details map<text, text>, " +
+            "            place_rank int, " +
+            "            importance double, " +
+            "            wkt text, " +
+            "            PRIMARY KEY ((address_id, account_id, longitude, latitude)) " +
+            ");";
+
+    public static final String KEYSPACE_CREATE_TYPE_POLYGON = "CREATE TYPE IF NOT EXISTS polygon (latitude double, longitude double);";
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        createCassandraKeyspace();
+    }
+
+    public void createCassandraKeyspace() {
+        try {
+            EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+        Cluster cluster = Cluster.builder().addContactPoints("127.0.0.1").withPort(9042).build();
+        Session session = cluster.connect();
+        session.execute(KEYSPACE_CREATION_QUERY);
+        session.execute("DROP TABLE IF EXISTS geoKeySpace.address_data");
+        session.execute(KEYSPACE_ACTIVATE_QUERY);
+        session.execute(KEYSPACE_CREATE_TYPE_POLYGON);
+        session.execute(KEYSPACE_CREATE_ADDRESS_DATA);
+    }
+}
