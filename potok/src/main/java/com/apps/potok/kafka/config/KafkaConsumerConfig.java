a926499a44fabaeeef3e8ccc744bdd5906011ea1
georgy.saukov <gsaukov@yahoo.com>
2020-04-19 01:34:22 +0200

    kafka setup

--- potok/src/main/java/com/apps/potok/kafka/config/KafkaConsumerConfig.java
+++ potok/src/main/java/com/apps/potok/kafka/config/KafkaConsumerConfig.java
@@ -5,17 +5,15 @@ import java.util.Map;
 
 import com.apps.potok.soketio.model.execution.Execution;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @EnableKafka
@@ -27,24 +25,24 @@ public class KafkaConsumerConfig {
     @Value(value = "${kafka.clusterAddress}")
     private String clusterAddress;
 
-    public ConsumerFactory<String, Execution> consumerFactory(String groupId) {
+    public ConsumerFactory<String, Execution> consumerFactory(String groupId, Deserializer keyDeserializer,
+                                                              Deserializer valueDeserializer) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, clusterAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+        return new DefaultKafkaConsumerFactory<>(props, keyDeserializer, valueDeserializer);
     }
 
-    public ConcurrentKafkaListenerContainerFactory<String, Execution> kafkaListenerContainerFactory(String groupId) {
+    public ConcurrentKafkaListenerContainerFactory<String, Execution> kafkaListenerContainerFactory
+            (String groupId, Deserializer keyDeserializer, Deserializer valueDeserializer) {
         ConcurrentKafkaListenerContainerFactory<String, Execution> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory(groupId));
+        factory.setConsumerFactory(consumerFactory(groupId, keyDeserializer, valueDeserializer));
         return factory;
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Execution> depositKafkaListenerContainerFactory() {
-        return kafkaListenerContainerFactory(GROUPID_DEPOSIT);
+        return kafkaListenerContainerFactory(GROUPID_DEPOSIT, new StringDeserializer(), new JsonDeserializer<>(Execution.class));
     }
 
 }
