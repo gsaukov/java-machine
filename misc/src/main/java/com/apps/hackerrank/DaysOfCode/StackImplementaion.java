d27491c4f5ad0b04d8979c0f257c3e74aacc52b2
georgy.saukov <gsaukov@yahoo.com>
2020-07-19 23:50:31 +0200

    Hackerrank days of code stack

---
+++ misc/src/main/java/com/apps/hackerrank/DaysOfCode/StackImplementaion.java
@@ -0,0 +1,11 @@
+package com.apps.hackerrank.DaysOfCode;
+
+public class StackImplementaion {
+
+    public void pushCharacter(Object o){}
+    public void enqueueCharacter(Object o){}
+    public void popCharacter(Object o){}
+    public void dequeueCharacter(Object o){}
+
+
+}
