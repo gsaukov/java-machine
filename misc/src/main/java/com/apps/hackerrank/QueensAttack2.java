587351d9046e0224720ae963d2f7a154f2464e0e
georgy.saukov <gsaukov@yahoo.com>
2020-10-20 00:12:44 +0200

    Hackerrank

--- misc/src/main/java/com/apps/hackerrank/QueensAttack2.java
+++ misc/src/main/java/com/apps/hackerrank/QueensAttack2.java
@@ -7,6 +7,7 @@ package com.apps.hackerrank;
 
 public class QueensAttack2 {
 
+    private static P Q;
     private static P NN;
     private static P NE;
     private static P EE;
@@ -17,28 +18,85 @@ public class QueensAttack2 {
     private static P WN;
 
     static int queensAttack(int n, int k, int qY, int qX, int[][] obstacles) {
-        NN = new P(qX, n);
-        int min_NE = Math.min((n-qX), (n-qY));
-        NE = new P(qX + min_NE, qY + min_NE);
-        EE = new P(n, qY);
-        int min_ES = Math.min((n-qX), (qY - 1));
-        ES = new P(qX + min_ES, qY - min_ES);
-        SS = new P(qX, 1);
-        int min_SW = Math.min((qX - 1), (qY - 1));
-        SW = new P(qX - min_ES, qY - min_ES);
-        WW = new P(1, qY);
-        int min_WN = Math.min((qX - 1), (n-qY));
-        WN = new P(qX - min_ES, qY + min_ES);
+        Q = new P(qX, qY);
+        initBorders(n);
+        applyObstacles(obstacles);
         return 0;
     }
 
-    public static void main(String[] args)  {
-        System.out.println(queensAttack(8,0, 4,4,null));
-        System.out.println(collinear(40,40, 50,30,70,10));
+    static void initBorders(int n){
+        NN = new P(Q.x, n);
+        int min_NE = Math.min((n-Q.x), (n-Q.y));
+        NE = new P(Q.x + min_NE, Q.y + min_NE);
+        EE = new P(n, Q.y);
+        int min_ES = Math.min((n-Q.x), (Q.y - 1));
+        ES = new P(Q.x + min_ES, Q.y - min_ES);
+        SS = new P(Q.x, 1);
+        int min_SW = Math.min((Q.x - 1), (Q.y - 1));
+        SW = new P(Q.x - min_ES, Q.y - min_ES);
+        WW = new P(1, Q.y);
+        int min_WN = Math.min((Q.x - 1), (n-Q.y));
+        WN = new P(Q.x - min_ES, Q.y + min_ES);
+    }
+
+
+    static void applyObstacles(int[][] obstacles) {
+        for(int i = 0; i < obstacles.length; i++) {
+            P o = new P(obstacles[i][1], obstacles[i][0]);
+            if (isBetween(NN, o)) {
+                NN = o;
+            } else if (isBetween(NE, o)) {
+                NE = o;
+            } else if (isBetween(EE, o)) {
+                EE = o;
+            } else if (isBetween(ES, o)) {
+                ES = o;
+            } else if (isBetween(SS, o)) {
+                SS = o;
+            } else if (isBetween(SW, o)) {
+                SW = o;
+            } else if (isBetween(WW, o)) {
+                WW = o;
+            } else if (isBetween(WN, o)) {
+                WN = o;
+            }
+        }
+    }
+
+    private static boolean isBetween(P p, P o) {
+        int dxc = o.x - p.x;
+        int dyc = o.y - p.y;
+
+        int dxl = Q.x - p.x;
+        int dyl = Q.y - p.y;
+
+        if ((dxc * dyl - dyc * dxl) != 0)
+            return false;
+
+        if (Math.abs(dxl) >= Math.abs(dyl))
+            return dxl > 0 ?
+                    p.x <= o.x && o.x <= Q.x :
+                    Q.x <= o.x && o.x <= p.x;
+        else
+            return dyl > 0 ?
+                    p.y <= o.y && o.y <= Q.y :
+                    Q.y <= o.y && o.y <= p.y;
     }
 
     static boolean collinear(int x1, int y1, int x2, int y2, int x3, int y3) {
         return (y1 - y2) * (x1 - x3) == (y1 - y3) * (x1 - x2);
+    }
+
+
+
+    public static void main(String[] args)  {
+        int[][] obs = {{3, 5}, {6, 4}, {6, 4}, {4, 5}, {7, 7}, {6, 6}};
+        System.out.println(queensAttack(8,obs.length, 4,4, obs));
+//        System.out.println(collinear(50,30,70,10,40,40));
+//        Q = new P(40, 40);
+//        P p = new P(50,30);
+//        P o = new P(70,10);
+//        System.out.println(isBetween(p, o));
     }
 
     public static class P {
