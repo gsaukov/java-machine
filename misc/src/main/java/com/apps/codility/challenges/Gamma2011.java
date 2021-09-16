d5277df676460ca57a42d7ce33617a3f637b4879
georgy.saukov <gsaukov@yahoo.com>
2021-09-16 21:18:27 +0200

    codility challenges Gamma2011 crazy

--- misc/src/main/java/com/apps/codility/challenges/Gamma2011.java
+++ misc/src/main/java/com/apps/codility/challenges/Gamma2011.java
@@ -34,7 +34,9 @@ string S consists only of lowercase letters (a−z).
 *
 */
 
-import java.util.LinkedList;
+/*
+* @see com.apps.hackerrank.t24test.PalindromeCounter
+*/
 
 public class Gamma2011 {
 
@@ -44,29 +46,79 @@ public class Gamma2011 {
     }
 
     public static int solution(String S) {
-        LinkedList<Character> queue = new LinkedList<>();
-        char[] A = S.toCharArray();
-        int palindromeCount = 0;
-        for(int i = 0; i < A.length; i++) {
-            if(!queue.isEmpty() && queue.getLast().equals(A[i])) {
-                queue.pollLast();
-                palindromeCount++;
-            } else {
-                queue.add(A[i]);
+        char str[] = S.toCharArray();
+        int n = str.length;
+
+        // create empty 2-D matrix that counts all
+        // palindrome substring. dp[i][j] stores counts of
+        // palindromic substrings in st[i..j]
+        int dp[][] = new int[n][n];
+
+        // P[i][j] = true if substring str[i..j] is
+        // palindrome, else false
+        boolean P[][] = new boolean[n][n];
+
+        // palindrome of single length
+        for (int i = 0; i < n; i++)
+            P[i][i] = true;
+
+        // palindrome of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (str[i] == str[i + 1]) {
+                P[i][i + 1] = true;
+                dp[i][i + 1] = 1;
             }
         }
 
-        queue = new LinkedList<>();
+        // Palindromes of length more than 2. This loop is
+        // similar to Matrix Chain Multiplication. We start
+        // with a gap of length 2 and fill the DP table in a
+        // way that gap between starting and ending indexes
+        // increases one by one by outer loop.
+        for (int gap = 2; gap < n; gap++) {
+            // Pick starting point for current gap
+            for (int i = 0; i < n - gap; i++) {
+                // Set ending point
+                int j = gap + i;
 
-        for(int i = A.length - 1; i >= 0 ; i--) {
-            if(!queue.isEmpty() && queue.getLast().equals(A[i])) {
-                queue.pollFirst();
-                palindromeCount++;
-            } else {
-                queue.add(A[i]);
+                // If current string is palindrome
+                if (str[i] == str[j] && P[i + 1][j - 1])
+                    P[i][j] = true;
+
+                // Add current palindrome substring ( + 1)
+                // and rest palindrome substring (dp[i][j-1]
+                // + dp[i+1][j]) remove common palindrome
+                // substrings (- dp[i+1][j-1])
+                if (P[i][j] == true)
+                    dp[i][j] = dp[i][j - 1] + dp[i + 1][j]
+                            + 1 - dp[i + 1][j - 1];
+                else
+                    dp[i][j] = dp[i][j - 1] + dp[i + 1][j]
+                            - dp[i + 1][j - 1];
             }
         }
+        printMatrix(dp);
+        printMatrix(P);
 
-        return palindromeCount;
+        // return total palindromic substrings
+        return dp[0][n - 1];
+    }
+
+    private static void printMatrix(int A[][]) {
+        for(int row[] : A) {
+            System.out.println();
+            for(int n : row) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+
+    private static void printMatrix(boolean A[][]) {
+        for(boolean row[] : A) {
+            System.out.println();
+            for(boolean n : row) {
+                System.out.print(n + " ");
+            }
+        }
     }
 }
