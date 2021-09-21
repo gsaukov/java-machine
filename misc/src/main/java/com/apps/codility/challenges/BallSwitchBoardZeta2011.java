9da88d3515d383589b24a528bb5d12869dc971f1
georgy.saukov <gsaukov@yahoo.com>
2021-09-21 22:44:04 +0200

    codility challenges BallSwitchBoard Zeta2011

---
+++ misc/src/main/java/com/apps/codility/challenges/BallSwitchBoardZeta2011.java
@@ -0,0 +1,18 @@
+package com.apps.codility.challenges;
+
+public class BallSwitchBoardZeta2011 {
+
+    public static void main(String[] args) {
+        int[][] A = new int[][]{{-1, 0, -1},{-1, 0, 0}};
+        System.out.println(solution(A, 4));
+    }
+
+    public static int solution(int[][] A, int K) {
+        int bottom = 0;
+        for (int i = 0; i <= K; i++) {
+
+        }
+        return bottom;
+    }
+
+}
