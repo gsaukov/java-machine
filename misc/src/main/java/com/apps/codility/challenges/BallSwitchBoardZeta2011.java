3747edd8ca2938514a9728ea33688b29c0bee7d8
georgy.saukov <gsaukov@yahoo.com>
2021-09-21 23:22:18 +0200

    codility challenges BallSwitchBoard Zeta2011

--- misc/src/main/java/com/apps/codility/challenges/BallSwitchBoardZeta2011.java
+++ misc/src/main/java/com/apps/codility/challenges/BallSwitchBoardZeta2011.java
@@ -3,6 +3,8 @@ package com.apps.codility.challenges;
 public class BallSwitchBoardZeta2011 {
     private static int BOTTOM_BORDER;
     private static int RIGHT_BORDER;
+    private static final int DIRECTION_BOTTOM = -1;
+    private static final int DIRECTION_RIGHT = 1;
 
     public static void main(String[] args) {
         int[][] A = new int[][]{{-1, 0, -1}, {-1, 0, 0}};
@@ -16,12 +18,12 @@ public class BallSwitchBoardZeta2011 {
         int x = 0;
         int y = 0;
         for (int i = 0; i <= K; i++) {
-            bottom += move(A, x, y);
+            bottom += move(A, x, y, DIRECTION_BOTTOM);
         }
         return bottom;
     }
 
-    private static int move(int[][] A, int x, int y) {
+    private static int move(int[][] A, int x, int y, int direction) {
         while (true) {
             if (y > BOTTOM_BORDER) {
                 return 1;
@@ -30,8 +32,12 @@ public class BallSwitchBoardZeta2011 {
                 return 0;
             }
 
-            if(A[x][y] == 1) {
-
+            if (A[x][y] == DIRECTION_BOTTOM) {
+                A[x][y] = DIRECTION_RIGHT;
+                move(A, x, y + 1, DIRECTION_BOTTOM);
+            } else if (A[x][y] == DIRECTION_RIGHT) {
+                A[x][y] = DIRECTION_BOTTOM;
+                move(A, x, y + 1, DIRECTION_BOTTOM);
             }
         }
     }
