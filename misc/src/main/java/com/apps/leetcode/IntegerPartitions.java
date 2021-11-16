3513eb886068a0be4bd35b261aef1fdc755f4a98
georgy.saukov <gsaukov@yahoo.com>
2021-11-16 23:07:25 +0100

    leetcode IntegerPartitions

---
+++ misc/src/main/java/com/apps/leetcode/IntegerPartitions.java
@@ -0,0 +1,27 @@
+package com.apps.leetcode;
+
+import java.util.Arrays;
+
+public class IntegerPartitions {
+
+//    https://leetcode.com/problems/array-partition-i/
+
+    public static void main(String[] args) {
+        int[] arr = {6,2,6,5,1,2};
+        System.out.println(arrayPairSum(arr));
+    }
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxMinSum = 0;
+
+        for (int i = 1; i < nums.length;) {
+            int curMin = Math.min(nums[i-1], nums[i]);
+            maxMinSum = maxMinSum + curMin;
+            i = i + 2;
+        }
+
+        return maxMinSum;
+    }
+
+}
