43a20860449c5133769137c661342642c2e0a124
georgy.saukov <gsaukov@yahoo.com>
2021-11-20 00:05:47 +0100

    leetcode: CountSubArraysWithSumK

--- misc/src/main/java/com/apps/leetcode/CountSubArraysWithSumK.java
+++ misc/src/main/java/com/apps/leetcode/CountSubArraysWithSumK.java
@@ -5,22 +5,18 @@ import java.util.Map;
 
 public class CountSubArraysWithSumK {
 //    https://leetcode.com/problems/subarray-sum-equals-k/
-    private static Map<Integer, Integer> prefSum = new HashMap<>();
+// static is not accepted by leetcode be carefull
 
     public static void main(String[] args) {
-        int[] arr = {-1,-1,1};
-        System.out.println(subarraySum(arr, 0));
+        int[] arr = { 1, 2, 3 };
+        System.out.println(subarraySum(arr, 3));
     }
 
     public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefSum = new HashMap<>();
         int res = 0;
         int curSum = 0;
 
-        if(nums.length == 1 && nums[0] == k) { //edge case
-            return 1;
-        }
-
-        curSum = 0;
         for (int i = 0; i < nums.length; i++) {
             curSum = curSum + nums[i];
             if (curSum == k) {
@@ -31,12 +27,12 @@ public class CountSubArraysWithSumK {
                 res += prefSum.get(curSum - k);
             }
 
-            addToPrefSum(curSum);
+            addToPrefSum(prefSum, curSum);
         }
         return res;
     }
 
-    private static void addToPrefSum (int curSum) {
+    private static void addToPrefSum (Map<Integer, Integer> prefSum, int curSum) {
         if (prefSum.containsKey(curSum)) {
             prefSum.put(curSum, prefSum.get(curSum) + 1);
         } else {
