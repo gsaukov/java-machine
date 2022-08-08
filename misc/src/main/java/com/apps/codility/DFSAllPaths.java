ab575a9dbea2c85ed631c6ba5d0941478038f86a
Georgy Saukov <georgy.saukov@sap.com>
2022-08-08 18:55:54 +0200

    Dfs req 2.

--- misc/src/main/java/com/apps/codility/DFSAllPaths.java
+++ misc/src/main/java/com/apps/codility/DFSAllPaths.java
@@ -24,37 +24,16 @@ public class DFSAllPaths {
         graph.put("F1", new HashSet<>(asList("F", "F2")));
         graph.put("F2", new HashSet<>(asList("F", "F1")));
 
-
-        List<String> path = dfsReq("A", "E", new HashSet<>());
+        List<String> path = new ArrayList<>();
+//        path = dfsReq("A", "K", new HashSet<>());
+        dfsReq2("A", "K", new HashSet<>(), path);
         for(String node : path) {
             System.out.println(node);
         }
     }
 
-    private static List<List<String>> bfsAllPaths(HashMap<String, Set<String>> graph, String start, String target) {
-//
-//        List<List<String>> res = new ArrayList<>();
-//        LinkedList<QueueItem> queue = new LinkedList<>();
-//        queue.push(new QueueItem(start, new HashSet<>(asList(start))));
-
-//        graph.get()
-//        (vertex, path) = queue.pop(0)
-//        for next in graph[vertex] - set(path):
-//        while (!queue.isEmpty()) {
-//            QueueItem item = queue.pop();
-//            item.ribs.removeAll()
-//            for next in graph[vertex] - set(path):
-//                if next == goal:
-//                    yield path + [next]
-//                else:
-//                    queue.append((next, path + [next]))
-//
-//        }
-        return null;
-    }
-
     private static List<String> dfsReq(String current, String end, Set<String> visited) {
-        Set<String> nodes = graph.get(current);
+        Set<String> nodes = new HashSet<>(graph.get(current));
         nodes.removeAll(visited);
         for(String node: nodes) {
             visited.add(node);
@@ -73,6 +52,24 @@ public class DFSAllPaths {
         return null;
     }
 
+    private static boolean dfsReq2(String current, String end, Set<String> visited, List<String> res) {
+        Set<String> nodes = new HashSet<>(graph.get(current));
+        nodes.removeAll(visited);
+        boolean found = false;
+        for(String node: nodes) {
+            if(node.equals(end)) {
+                res.add(end);
+                found = true;
+            } else {
+                visited.add(node);
+                if(dfsReq2(node, end, visited, res)) {
+                    found = true;
+                    res.add(node);
+                }
+            }
+        }
+        return found;
+    }
 
 
 
