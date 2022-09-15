8c15f07704ac13ab7b486bbf9eca7f0099bb2e7e
Georgy Saukov <georgy.saukov@sap.com>
2022-09-15 16:59:43 +0200

    leetcode Search in rotated array

--- misc/src/main/java/com/apps/leetcode/SearchInRotatedSortedArray.java
+++ misc/src/main/java/com/apps/leetcode/SearchInRotatedSortedArray.java
@@ -18,29 +18,29 @@ public class SearchInRotatedSortedArray {
         int left;
         int right;
 
-        if(breach == -1){
+        if (breach == -1) {
             left = 0;
-            right = nums.length-1;
+            right = nums.length - 1;
         } else if (target >= nums[0] && target <= nums[breach]) {
             left = 0;
             right = breach;
         } else {
             left = breach + 1;
-            right = nums.length-1;
+            right = nums.length - 1;
         }
 
         int mid = (left + right) / 2;
         while (left < right - 1) {
 
-            if (nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
             }
 
-            if (nums[left] == target){
+            if (nums[left] == target) {
                 return left;
             }
 
-            if (nums[right] == target){
+            if (nums[right] == target) {
                 return right;
             }
 
@@ -60,15 +60,15 @@ public class SearchInRotatedSortedArray {
         int left = 0;
         int right = nums.length - 1;
         int mid = (left + right) / 2;
-        if(nums[left] < nums[right]){
+        if (nums[left] < nums[right]) {
             return -1; // already sorted. no breach.
         }
 
         while (left < right) {
-            if (nums[mid] > nums[mid + 1]){
+            if (nums[mid] > nums[mid + 1]) {
                 return mid;
             }
-            if (nums[mid] < nums[mid - 1]){
+            if (nums[mid] < nums[mid - 1]) {
                 return mid - 1;
             }
 
@@ -80,7 +80,6 @@ public class SearchInRotatedSortedArray {
                 mid = (left + right) / 2;
             }
         }
-
 
         return mid;
     }
