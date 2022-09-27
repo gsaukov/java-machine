642dd962970f2a55c7aff3a0b568c629e232f302
Georgy Saukov <georgy.saukov@sap.com>
2022-09-27 19:34:08 +0200

    leetcode Sum of distances in tree.

---
+++ misc/src/main/java/com/apps/leetcode/SumOfDistancesInTree.java
@@ -0,0 +1,63 @@
+package com.apps.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//https://leetcode.com/problems/sum-of-distances-in-tree/
+public class SumOfDistancesInTree {
+
+    Map<Integer, Node> tree = new HashMap<>();
+
+    public static void main(String[] args) {
+        int[][] A = {{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}};
+        SumOfDistancesInTree s = new SumOfDistancesInTree();
+        s.sumOfDistancesInTree(6, A);
+    }
+
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        toTree(edges);
+        int[] res = new int[tree.size()];
+        for(Integer nodeId : tree.keySet()) {
+        }
+        return null;
+    }
+
+    private void toTree(int[][] edges) {
+        for (int[] r : edges) {
+            int from = r[0];
+            int to = r[1];
+            addNodeIfMissing(from);
+            addNodeIfMissing(to);
+            addReferenceFromTo(from, to);
+        }
+    }
+
+    private void addNodeIfMissing(int from) {
+        if (!tree.containsKey(from)) {
+            tree.put(from, new Node(from));
+        }
+    }
+
+    private void addReferenceFromTo(int from, int to) {
+        tree.get(from).addChild(tree.get(to));
+    }
+
+    private static class Node {
+        int val;
+        List<Node> children;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public void addChild(Node child) {
+            if (children == null) {
+                this.children = new ArrayList<>();
+            }
+            children.add(child);
+        }
+    }
+
+}
