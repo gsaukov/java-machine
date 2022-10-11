00bacaba13fb5a5afd9a6ecd549522029f0b20e0
Georgy Saukov <georgy.saukov@sap.com>
2022-10-12 00:49:55 +0200

    Java version 17 testng context reconfiguration.

--- potok/src/test/java/com/apps/potok/config/BaseTest.java
+++ potok/src/test/java/com/apps/potok/config/BaseTest.java
@@ -1,10 +1,10 @@
 package com.apps.potok.config;
 
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
-@ContextConfiguration(classes = {PotokTestConfiguration.class}, initializers = {ConfigFileApplicationContextInitializer.class})
+@ContextConfiguration(classes = {PotokTestConfiguration.class}, initializers = {ConfigDataApplicationContextInitializer.class})
 public class BaseTest extends AbstractTestNGSpringContextTests {
 
 }
