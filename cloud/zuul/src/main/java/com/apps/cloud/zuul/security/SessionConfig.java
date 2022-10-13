649f37db56fe2740f99547f406ddadc4cc6e4662
Georgy Saukov <georgy.saukov@sap.com>
2022-10-13 23:06:57 +0200

    Java version 17 zull, Spring Cloud 2021.0.1 fixes. LettuceConnectionFactory for redis.

--- cloud/zuul/src/main/java/com/apps/cloud/zuul/security/SessionConfig.java
+++ cloud/zuul/src/main/java/com/apps/cloud/zuul/security/SessionConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
@@ -17,12 +17,11 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
     private Environment env;
 
     @Bean
-    public JedisConnectionFactory connectionFactory() {
-        redis.clients.jedis.util.SafeEncoder f;
+    public LettuceConnectionFactory connectionFactory() {
         RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration();
         redisConf.setHostName(env.getProperty("spring.redis.host"));
         redisConf.setPort(Integer.parseInt(env.getProperty("spring.redis.port")));
-        return new JedisConnectionFactory(redisConf);
+        return new LettuceConnectionFactory(redisConf);
     }
 
 }
