f20d138d2ddbb2a9a6aeb0371257fb707914b823
Georgy Saukov <georgy.saukov@sap.com>
2023-05-09 18:38:54 +0200

    hackerrank bigger is greater

---
+++ misc/src/main/java/com/apps/reflection/TypeScanner.java
@@ -0,0 +1,105 @@
+package com.apps.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class TypeScanner {
+
+
+    /*
+    * Scanner used to build reference map with links to targetClass in scanned clazz;
+    */
+    public static Map<String, Object> scan(Class targetClass, Class clazz) {
+        TypeScanner scanner = new TypeScanner(targetClass);
+        return scanner.scanClass(clazz.getSimpleName(), clazz, new HashSet<>());
+    }
+
+    private Class targetClass;
+
+    private TypeScanner(final Class targetClass) {
+        this.targetClass = targetClass;
+    }
+
+    private Map<String, Object> scanClass(String parentName, Class clazz, Set<Class> visited) {
+        Map<String, Object> res = new HashMap<>();
+
+        if (targetClass.isAssignableFrom(clazz)) {
+            res.put(parentName, clazz);
+            return res;
+        } else if (visited.contains(clazz)) {
+            return res;
+        }
+
+        visited.add(clazz);
+
+        for (Field field : getFields(clazz)) {
+            Class<?> type = field.getType();
+            String name = parentName + "." + field.getName()+ "(" + type.getSimpleName() + ")";
+            if (type.isEnum() || type.isPrimitive()) {
+                continue;
+            } else if (Iterable.class.isAssignableFrom(type)) {
+                Map<String, Object> scanRes = scanList(name, field.getGenericType(), new HashSet<>(visited));
+                if (!scanRes.isEmpty()) {
+                    res.put(name, scanRes);
+                }
+            } else if (Map.class.isAssignableFrom(type)) {
+                Map<String, Object> scanRes = scanMap(name, field.getGenericType(), new HashSet<>(visited));
+                if (!scanRes.isEmpty()) {
+                    res.put(name, scanRes);
+                }
+            } else {
+                Map<String, Object> scanRes = scanClass(name, type, new HashSet<>(visited));
+                if (!scanRes.isEmpty()) {
+                    res.put(name, scanRes);
+                }
+            }
+        }
+        return res;
+    }
+
+    private Map<String, Object> scanList(String parentName, Type genericType, Set<Class> visited) {
+        ParameterizedType listType = getParameterizedType(genericType);
+        if (listType != null) {
+            Type value = listType.getActualTypeArguments()[0];
+            if (value instanceof Class) {//could be sun.reflect.generics.reflectiveObjects.TypeVariableImpl which is not Class
+                return scanClass(parentName, (Class) value, visited);
+            }
+        }
+        return Collections.emptyMap();
+    }
+
+    private Map<String, Object> scanMap(String parentName, Type genericType, Set<Class> visited) {
+        ParameterizedType mapTypes = getParameterizedType(genericType);
+        if (mapTypes != null) {
+            Type key = mapTypes.getActualTypeArguments()[0];
+            Type value = mapTypes.getActualTypeArguments()[1];
+            if (value instanceof Class) { //could be sun.reflect.generics.reflectiveObjects.TypeVariableImpl which is not Class
+                return scanClass(parentName, (Class) value, visited);
+            }
+        }
+        //Nothing found fallback.
+        return Collections.emptyMap();
+    }
+
+    private ParameterizedType getParameterizedType(Type c) { //Support of legacy collections Hashtable
+        while (c != null && c != Object.class) {
+            if (ParameterizedType.class.isAssignableFrom(c.getClass())) {
+                return (ParameterizedType) c;
+            }
+            c = ((Class) c).getGenericSuperclass();
+        }
+        return null;
+    }
+
+    private List<Field> getFields(Class c) {
+        List<Field> fields = new ArrayList<>();
+        while (c != null && c != Object.class) {//for interfaces super class is null.
+            fields.addAll(Arrays.asList(c.getDeclaredFields()));
+            c = c.getSuperclass();
+        }
+        return fields;
+    }
+
+}
