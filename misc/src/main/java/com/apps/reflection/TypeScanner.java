6d17702c35b444e25d174aab4c61d2c56fc52204
Georgy Saukov <georgy.saukov@sap.com>
2023-05-13 21:44:36 +0200

    Type scanner flatter import fix

--- misc/src/main/java/com/apps/reflection/TypeScanner.java
+++ misc/src/main/java/com/apps/reflection/TypeScanner.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TypeScanner {
 
